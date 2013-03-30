00c1fb0c1cc1505b8e0ad8a7e6310bdb78eeaf6f
Damiano Braga <dbraga@trulia.com>
2013-03-30 00:01:00 -0700

    fixed typo

--- app.js
+++ app.js
@@ -34,14 +34,15 @@ app.get('/', function (req, res) {
 
 });
 
+var client;
 
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  client = require("redis").createClient(rtg.port, rtg.hostname);
 
   redis.auth(rtg.auth.split(":")[1]); 
 } else {
-var redis = require("redis").createClient();
+ client = require("redis").createClient();
 }
 
 client.on("error", function (err) {
