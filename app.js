c46668e9c28338710ec54265deedabb7ef478113
Damiano Braga <dbraga@trulia.com>
2013-03-29 23:58:53 -0700

    redis default conf

--- app.js
+++ app.js
@@ -35,8 +35,14 @@ app.get('/', function (req, res) {
 });
 
 
-var redis = require("redis"),
-    client = redis.createClient();
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+  redis.auth(rtg.auth.split(":")[1]); 
+} else {
+var redis = require("redis").createClient();
+}
 
 client.on("error", function (err) {
     console.log("error event - " + client.host + ":" + client.port + " - " + err);
